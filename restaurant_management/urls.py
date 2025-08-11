39e6df973e9a11d1c3da0b9e9bad77cfe6dd3632
Rohit Verma <127671850+rohit-verma-11@users.noreply.github.com>
2025-08-11 15:07:33 +0530

    28745

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from .views import *
+from django.conf.urls import handler404
+
+handler404 = custom_page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
