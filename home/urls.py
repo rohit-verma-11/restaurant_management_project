7ccca2d0553d60be610821ace269c9685349be52
Rohit Verma <127671850+rohit-verma-11@users.noreply.github.com>
2025-08-11 17:30:52 +0530

    30151

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from home.views import homepage, about_page, menu_view
 urlpatterns = [
     path('' , homepage , name='homepage'),
     path('about/',about_page,name='about'),
-    path('menu/',menu_view,name='menu'),
+    path('menu/',menu_view,name='menu')
 ]
