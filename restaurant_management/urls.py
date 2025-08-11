12e52b42942b35485c4805066348ab90e69baca5
Rohit Verma <127671850+rohit-verma-11@users.noreply.github.com>
2025-08-11 15:21:07 +0530

    28745

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -23,9 +23,9 @@ handler404 = custom_page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'),name="home"),
+    path('',include('home.urls'),name="home"),
     path('api/',include('home.urls'),name='api'),
-    path('api/accounts/',include('account.urls')),
-    path('api/products/',include('products.urls')),
-    path('api/orders/',include('orders.urls')),
+    path('api/accounts/',include('account.urls'),name='api_accounts'),
+    path('api/products/',include('products.urls'),name='api_products'),
+    path('api/orders/',include('orders.urls'),name='api_urls'),
 ]
